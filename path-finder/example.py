f28edc2adb915a038155b0b26f76355a4db8c6f4
Qomp4ss <imagggine@gmail.com>
2024-12-22 22:31:04 +0100

    Deleting the example file

--- path-finder/example.py
+++
@@ -1,9 +0,0 @@
-from operators import PathFinder
-from loaders import PDLoader
-
-    
-pf = PathFinder({r'D:\\':True})
-files = pf.find("*", ".csv|.txt|.xlsx", name_type="glob", ext_type = "regex")
-scoped_files = files.select_paths("Forex", "isin").groupby("name")
-eur_pln = scoped_files['eurpln'].load(PDLoader)
-    
