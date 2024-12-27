b9841a815aa761ccc73761449fc42faad44404a1
Qomp4ss <imagggine@gmail.com>
2024-12-27 21:59:52 +0100

    Renaming the folder

--- path-finder/__init__.py
+++
@@ -1,9 +0,0 @@
-#
-from .abc_loader import ABLoader, BaseLoader
-from .loaders import PDLoader
-from .operators import PathFinder
-
-__all__ = ['PathFinder','ABLoader', 'BaseLoader', 'PDLoader']
-
-__version__ = "0.1.0"
-__author__ = "Qomp4ss"
