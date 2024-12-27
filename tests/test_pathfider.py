9c1908f0cdbf9b9a4b105a57411a979246a0ec47
Qomp4ss <imagggine@gmail.com>
2024-12-27 23:56:38 +0100

    adding test function for find method with  subdirectory traversal

--- tests/test_pathfider.py
+++ tests/test_pathfider.py
@@ -58,7 +58,7 @@ class TestPathFinder(unittest.TestCase):
         
         self.assertIsInstance(PathFinder(arg), PathFinder)
         
-        
+     
     def test_add_dirs(self):
         in_put = {str(Path(__file__).cwd().parent): True,
                   str(Path(__file__).cwd()): False}
@@ -84,10 +84,94 @@ class TestPathFinder(unittest.TestCase):
         pf.add_dirs(in_put)
         self.assertRaises(KeyError, pf.del_dirs, str(Path(__file__).cwd().parent))
 
+    @patch('fpath_finder.pathfinder.os.path.isdir')        
+    def test_find_arg_validation(self, mock_isdir):
+        directory = r"C:\mock_directory"
+        mock_isdir.side_effect = lambda path: True if path == directory else False
+        pf = PathFinder({directory: False})
+        
+        args = [
+            (1, 'x'),
+            ('*', 2,),
+            ('H1', 'c', 3, 'isin'),
+            ('H1', 'c', 'abcd', 'isin'),
+            ('eurgbp|gbpeur|EURGBP_H4', 'txt|csv', 'regex', 'abcd')
+            ]
+        expected = {
+           (1, 'x'): TypeError,
+            ('*', 2,):TypeError,
+            ('H1', 'c', 3, 'isin'): TypeError,
+            ('H1', 'c', 'abcd', 'isin'): ValueError,
+            ('eurgbp|gbpeur|EURGBP_H4', 'txt|csv', 'regex', 'abcd'): ValueError
+                }
+
+
+        for arg in args:
+            with self.subTest(arg = arg):
+                with self.assertRaises(expected[arg]):
+                    pf.find(*arg)
+
+    @patch('fpath_finder.pathfinder.os.walk')
+    @patch('fpath_finder.pathfinder.os.path.isdir')
+    @patch('fpath_finder.pathfinder.os.path.isfile')
+    def test_find_nested_dir(self, mock_isfile, mock_isdir, mock_walk):
+        directory = r"C:\mock_directory"
+        mock_isdir.side_effect = lambda path: True if path == directory else False
+        
+        mock_files = ['Forex.xlsx','EURGBP_H4.csv','EURGBP_M5.csv',
+                      'EURJPY_H1.csv','EURJPY_M30.csv', 'EURUSD_M5.csv',
+                      'audcad.txt','audchf.txt','audusd.txt',
+                      'cadaud.txt','cadeur.txt','cadjpy.txt',
+                      'cadpln.txt','cadusd.txt','chfgbp.txt',
+                      'chfpln.txt','chfusd.txt','euraud.txt',
+                      'eurchf.txt','eurgbp.txt','eurjpy.txt',
+                      'eurpln.txt','gbpcad.txt','gbpchf.txt',
+                      'gbpeur.txt','gbpjpy.txt','gbppln.txt',
+                      'jpyaud.txt','jpypln.txt','nzdusd.txt',
+                      'usdcad.txt','usdchf.txt','usdjpy.txt',
+                      'usdpln.txt','xaggbp.txt','xauchf.txt',
+                      'xaueur.txt','xaugbp.txt']
+
+            
+        mock_walk.return_value = sorted([
+            [directory, ['EUR', 'CURR'], [f]]  if 'xlsx' in f 
+            else ['\\'.join([directory, 'EUR']), [], [f]] if 'csv' in f 
+            else ['\\'.join([directory, 'CURR']), [], [f]] 
+            for f in mock_files
+            ])
+        mock_isfile.side_effect = lambda path: True if path.split('\\')[-1] in mock_files else False
+        
+        pf = PathFinder({directory: True})
+        args = [
+            ('.*_.*|F.*', '.x.*|.c.*', 'regex', 'regex'),
+            ('*', '*', 'glob', 'glob'),
+            ('H1', 'c', 'isin', 'isin'),
+            ('eurgbp|gbpeur|EURGBP_H4', 'txt|csv', 'regex', 'regex'),
+            ('Forex', '.xlsx', 'eq', 'eq')
+            ]
+        expected = {
+            ('.*_.*|F.*', '.x.*|.c.*', 'regex', 'regex'): [
+                'EURGBP_H4.csv', 'EURGBP_M5.csv','EURJPY_H1.csv',
+                'EURJPY_M30.csv','EURUSD_M5.csv', 'Forex.xlsx'
+                ],
+            ('*', '*', 'glob', 'glob'): mock_files,
+            ('H1', 'c', 'isin', 'isin'): ['EURJPY_H1.csv'],
+            ('eurgbp|gbpeur|EURGBP_H4', 'txt|csv', 'regex', 'regex'): [
+                'EURGBP_H4.csv', 'eurgbp.txt', 'gbpeur.txt'
+                ],
+            ('Forex', '.xlsx', 'eq', 'eq'): ['Forex.xlsx']
+                }
+        
+        for arg in args:
+            with self.subTest(arg = arg):
+                result = [p[0] for p in pf.find(*arg).paths]
+                self.assertCountEqual(result, expected[arg])
+       
+
     @patch('fpath_finder.pathfinder.os.scandir')
     @patch('fpath_finder.pathfinder.os.path.isdir')
     @patch('fpath_finder.pathfinder.os.path.isfile')
-    def test_find_flat(self, mock_isfile, mock_isdir, mock_scandir):
+    def test_find_flat_dir(self, mock_isfile, mock_isdir, mock_scandir):
         directory = r"C:\mock_directory"
         mock_isdir.side_effect = lambda path: True if path == directory else False
         
@@ -132,9 +216,7 @@ class TestPathFinder(unittest.TestCase):
             with self.subTest(arg = arg):
                 result = [p[0] for p in pf.find(*arg).paths]
                 self.assertCountEqual(result, expected[arg])
-        
-
-
+    
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(TestPathFinder('test_empty_init'))
@@ -148,7 +230,9 @@ def suite():
     suite.addTest(TestPathFinder('test_add_dirs'))
     suite.addTest(TestPathFinder('test_del_dirs_iterable_arg'))
     suite.addTest(TestPathFinder('test_del_dirs_bad_arg'))
-    suite.addTest(TestPathFinder('test_find_flat'))
+    suite.addTest(TestPathFinder('test_find_arg_validation'))
+    suite.addTest(TestPathFinder('test_find_nested_dir'))
+    suite.addTest(TestPathFinder('test_find_flat_dir'))
     return suite
 
 if __name__ == '__main__':
