6689225b701538e0bdc30f0543d7459b364b4f83
Qomp4ss <imagggine@gmail.com>
2024-12-29 00:26:34 +0100

    replacing re.match with re.search for path string compatibility

--- fpath_finder/matching.py
+++ fpath_finder/matching.py
@@ -8,7 +8,7 @@ def isin(string, pattern):
      return pattern in string
           
 def regex(string, pattern):
-      return re.match(pattern, string)
+      return re.search(pattern, string)
 
 def glob(string, pattern):
       return Path(string).match(pattern)
