7506a9f945855ddfeb0dd66b6b68a255878ca066
Qomp4ss <imagggine@gmail.com>
2025-01-01 18:12:26 +0100

    Adjusting the regex method to be in line with remaining matching methods and return bool values

--- fpath_finder/matching.py
+++ fpath_finder/matching.py
@@ -8,7 +8,7 @@ def isin(string, pattern):
      return pattern in string
           
 def regex(string, pattern):
-      return re.search(pattern, string)
+      return True if re.search(pattern, string) else False
 
 def glob(string, pattern):
       return Path(string).match(pattern)
