9858dc04e2fcc96f65b7007f65931a99720c14e4
Qomp4ss <imagggine@gmail.com>
2025-01-11 19:02:39 +0100

    Adding function for dynamic read of version

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from setuptools import setup, find_packages
-from .file_navigator.__init__ import __version__
+import os
+import re
+
+def find_version(file_path):
+    """Extract the version string from the specified file."""
+    with open(file_path, "r", encoding="utf-8") as f:
+        version_file = f.read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
 setup(
     name="file-navigator",
     description="Flexible directory scanner, file path manager, and customizable file loader—all in one",
     long_description=open("README.md").read(), 
     long_description_content_type="text/markdown",
-    version=__version__,
+    version=find_version("file_navigator/__init__.py"),
     license="MIT", 
     author="Qomp4ss",
     author_email="imaggine@gmail.com",
